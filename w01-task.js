ac2a85a2da046ddd7dbdb9ca0bdc677b9669122c
Ethan Ball <uthbees@gmail.com>
2024-02-28 10:48:53 -0700

    W01

---
+++ w01-task.js
@@ -0,0 +1,25 @@
+// 🔍 Part 1 error
+let userName = "Moroni";
+console.log(`Username: ${userName}`);
+userName = "Moronihah";
+console.log(`Username: ${userName}`);
+
+// 🔍 Part 2 error
+const currentDateAndTime = new Date().toString();
+console.log(`It is now ${currentDateAndTime}`);
+
+// 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments.
+
+let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(`The total is ${theTotal}`);
+
+// 'total' function declaration
+function total(...theNumbers) {
+	let sum = 0;
+	theNumbers.forEach((number) => {
+		// sum += aNumber * 1; // Why do we use * 1? It implicitly converts a string to a number.
+		// You should never actually do that though, since it's terrible for readability.
+		sum += parseInt(number)
+	})
+	return sum
+}
